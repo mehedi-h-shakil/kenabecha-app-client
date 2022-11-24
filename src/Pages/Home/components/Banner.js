b93f8cbc4bdf12de0b279dfa68785291290c2ad5
sneoo <epicshakil@gmail.com>
2022-11-24 20:52:57 +0600

    initial commit

---
+++ src/Pages/Home/components/Banner.js
@@ -0,0 +1,71 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="bg-base-200">
+      <div className="hero bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="carousel w-2/4 h-88">
+            <div id="slide1" className="carousel-item relative w-full">
+              <img
+                src="https://i.ibb.co/GMG6G9s/smartphones-1.jpg"
+                className="w-full"
+              />
+              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href="#slide4" className="btn btn-circle">
+                  ❮
+                </a>
+                <a href="#slide2" className="btn btn-circle">
+                  ❯
+                </a>
+              </div>
+            </div>
+            <div id="slide2" className="carousel-item relative w-full">
+              <img src="https://placeimg.com/800/200/arch" className="w-full" />
+              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href="#slide1" className="btn btn-circle">
+                  ❮
+                </a>
+                <a href="#slide3" className="btn btn-circle">
+                  ❯
+                </a>
+              </div>
+            </div>
+            <div id="slide3" className="carousel-item relative w-full">
+              <img src="https://placeimg.com/800/200/arch" className="w-full" />
+              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href="#slide2" className="btn btn-circle">
+                  ❮
+                </a>
+                <a href="#slide4" className="btn btn-circle">
+                  ❯
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="w-1/2">
+            <h1 className="text-5xl font-bold">Box Office News!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="Search your device"
+          className="input input-bordered w-96 my-10"
+        />
+        <button className="btn btn-outline ml-4">
+          <ion-icon name="search-outline" className="font-bold"></ion-icon>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
